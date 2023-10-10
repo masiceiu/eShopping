f20321c8f13dcb24df535494e173cc94238438ca
masiceiu <masiceiu@gmail.com>
2023-10-10 17:02:08 +0600

    2 files modified

--- src/app/firebase/core/firebase-core.module.ts
+++ src/app/firebase/core/firebase-core.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "@firebase/app";
 //import firebase from 'firebase/app';
 import { environment } from '../../../environments/environment';
 
-//const app = initializeApp(environment.firebase);
+const app = initializeApp(environment.firebase);
 @NgModule({
   imports: [
     CommonModule
